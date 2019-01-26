4f38d12acc768675f2ec04177db39f83a8886d0d
Mark Lannen <mark1.lannen@umontana.edu>
2019-01-26 11:27:38 -0700

    Added js file with console.log

---
+++ HW-2/script.js
@@ -0,0 +1,3 @@
+console.log("My 3 favorite restaurants are: 5 on Black, Caffe Dolce and Masala.")
+console.log("My 3 favorite snackfoods are: avacado on toast, popcorn and cheese on toast.")
+console.log("My 3 favorite fruits/vegatables are: huckleberries, kale and grapes.")
