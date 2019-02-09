f056f335cc0b28b7e367e3cf152ecca8a3c694dd
Mark Lannen <mark1.lannen@umontana.edu>
2019-02-09 13:20:44 -0700

    added third choice updated text box style to
    Also updated text box style to display 100% for all choices.

--- HW-4/script.js
+++ HW-4/script.js
@@ -4,11 +4,10 @@ function getChoice1 () {
   var userChoice = document.getElementById("choice").value;
   var myQuestion = document.getElementById("story");
 
-  console.log(userChoice);
-
-  if(userChoice === 'river') {
+  if(userChoice === 'river') 
+  {
     document.getElementById("choice-image").src="img/river-500px.jpg";
-    myQuestion.innerHTML = "Excellent Choice! You'll find a beautiful spot about 8 miles away. You've got all day to get there, so make sure you stop and take some pictures along the way."
+    myQuestion.innerHTML = "Excellent Choice! You'll find a beautiful spot about 8 miles away. You've got all day to get there, so make sure you stop and take some pictures along the way. Now, who do you want to take the lead your or your friend?"
     document.getElementById("choice").style.display="none";
     document.getElementById("btnSubmit").style.display="none"; 
 
@@ -18,7 +17,7 @@ function getChoice1 () {
   else if(userChoice === 'lake') 
   {
     document.getElementById("choice-image").src="img/lake-500px.jpg";
-    myQuestion.innerHTML = "A brave decision... you're in for a long day - check and make sure you have enough food before you start walking!"
+    myQuestion.innerHTML = "A brave decision... you're in for a long day - check and make sure you have enough food before you start walking! Now, who do you want to take the lead your or your friend?"
     document.getElementById("choice").style.display="none";
     document.getElementById("btnSubmit").style.display="none"; 
 
@@ -31,10 +30,27 @@ function getChoice1 () {
   }
 }
 
-// function getChoice2 () {
-//   var camp_choice = document.getElementById("choice2").value;
+function getChoice2 () {
+  var leader_choice = document.getElementById("choice2").value;
+  var myQuestion = document.getElementById("story");
 
-//   if (camp_choice === 'river') {
-//     myQuestion.innerHTML = 
-//   }
-// }
+  if (leader_choice = 'you')
+  {
+    myQuestion.innerHTML = "Make noise and keep an eye out for bears!"
+    document.getElementById("choice2").style.display="none";
+    document.getElementById("btnSubmit2").style.display="none"; 
+
+    document.getElementById("choice3").style.display="block";
+    document.getElementById("btnSubmit3").style.display="block"; 
+  }
+
+  if (leader_choice = 'friend')
+  {
+    myQuestion.innerHTML = "Remind your friend to make noise and keep an eye out for bears!" 
+    document.getElementById("choice2").style.display="none";
+    document.getElementById("btnSubmit2").style.display="none"; 
+
+    document.getElementById("choice3").style.display="block";
+    document.getElementById("btnSubmit3").style.display="block"; 
+  }
+}
