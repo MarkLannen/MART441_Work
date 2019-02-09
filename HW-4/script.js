0a3714c0f589ad6eb352c1e464de8130cad00698
Mark Lannen <mark1.lannen@umontana.edu>
2019-02-09 06:30:53 -0700

    zzz

--- HW-4/script.js
+++ HW-4/script.js
@@ -1,3 +1,16 @@
 console.log("My 3 favorite restaurants are: 5 on Black, Caffe Dolce and Masala.")
 console.log("My 3 favorite snackfoods are: avacado on toast, popcorn and cheese on toast.")
 console.log("My 3 favorite fruits/vegatables are: huckleberries, kale and grapes.")
+
+let season = 'summer';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+}
+
+else if (season === 'winter') {
+   console.log('It\'s winter! Everything is covered in snow.');
+
+} else {
+  console.log('Invalid season.');
+}
