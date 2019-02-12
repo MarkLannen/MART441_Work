3fdb9dbe6d3497e689508e8e8dc8ad17012cc10b
Mark Lannen <mark1.lannen@umontana.edu>
2019-02-11 21:28:34 -0700

    zzz

--- HW-4/script.js
+++ HW-4/script.js
@@ -6,27 +6,27 @@ function getChoice1 () {
 
   if(userChoice === 'river') 
   {
-    document.getElementById("choice-image").src="img/river-500px.jpg";
-    myQuestion.innerHTML = "Excellent Choice! You'll find a beautiful spot about 8 miles away. You've got all day to get there, so make sure you stop and take some pictures along the way. Now, who do you want to take the lead, or shoud you let your friend go first?"
-    document.getElementById("choice").style.display="none";
-    document.getElementById("btnSubmit").style.display="none"; 
+	document.getElementById("choice-image").src="img/river-500px.jpg";
+	myQuestion.innerHTML = "Excellent Choice! You'll find a beautiful spot about 8 miles away. You've got all day to get there, so make sure you stop and take some pictures along the way. Now, who do you want to take the lead, or shoud you let your friend go first?"
+	document.getElementById("choice").style.display="none";
+	document.getElementById("btnSubmit").style.display="none";
 
-    document.getElementById("choice2").style.display="block";
-    document.getElementById("btnSubmit2").style.display="block"; 
+	document.getElementById("choice2").style.display="block";
+	document.getElementById("btnSubmit2").style.display="block";
   }
   else if(userChoice === 'lake') 
   {
-    document.getElementById("choice-image").src="img/lake-500px.jpg";
-    myQuestion.innerHTML = "A brave decision... you're in for a long day - check and make sure you have enough food before you start walking! Now, who do you want to take the lead, or shoud you let your friend go first?"
-    document.getElementById("choice").style.display="none";
-    document.getElementById("btnSubmit").style.display="none"; 
+	document.getElementById("choice-image").src="img/lake-500px.jpg";
+	myQuestion.innerHTML = "A brave decision... you're in for a long day - check and make sure you have enough food before you start walking! Now, who do you want to take the lead, or shoud you let your friend go first?"
+	document.getElementById("choice").style.display="none";
+	document.getElementById("btnSubmit").style.display="none";
 
-    document.getElementById("choice2").style.display="block";
-    document.getElementById("btnSubmit2").style.display="block"; 
+	document.getElementById("choice2").style.display="block";
+	document.getElementById("btnSubmit2").style.display="block";
   }
   else
   {
-    myQuestion.innerHTML = "Please look a bit more closely at the options!"
+	myQuestion.innerHTML = "Please look a bit more closely at the options!"
   }
 }
 
@@ -36,31 +36,31 @@ function getChoice2 () {
 
   if (leader_choice === 'you')
   {
-    document.body.style.backgroundColor = "red";
-    document.getElementById("choice-image").src="img/bear-500px.jpg";
-    myQuestion.innerHTML = "Make noise and keep an eye out for bears! Now, do you want to eat before you start hiking?"
-    document.getElementById("choice2").style.display="none";
-    document.getElementById("btnSubmit2").style.display="none"; 
+	document.body.style.backgroundColor = "red";
+	document.getElementById("choice-image").src="img/bear-500px.jpg";
+	myQuestion.innerHTML = "Make noise and keep an eye out for bears! Now, do you want to eat before you start hiking?"
+	document.getElementById("choice2").style.display="none";
+	document.getElementById("btnSubmit2").style.display="none";
 
-    document.getElementById("choice3").style.display="block";
-    document.getElementById("btnSubmit3").style.display="block"; 
+	document.getElementById("choice3").style.display="block";
+	document.getElementById("btnSubmit3").style.display="block";
   }
 
   else if(leader_choice ==='friend')
   {
-    document.body.style.backgroundColor = "red";
-    document.getElementById("choice-image").src="img/bear-500px.jpg";
-    myQuestion.innerHTML = "Remind your friend to make noise and keep an eye out for bears! Now, do you want to eat before you start hiking?" 
-    document.getElementById("choice2").style.display="none";
-    document.getElementById("btnSubmit2").style.display="none"; 
+	document.body.style.backgroundColor = "red";
+	document.getElementById("choice-image").src="img/bear-500px.jpg";
+	myQuestion.innerHTML = "Remind your friend to make noise and keep an eye out for bears! Now, do you want to eat before you start hiking?"
+	document.getElementById("choice2").style.display="none";
+	document.getElementById("btnSubmit2").style.display="none";
 
-    document.getElementById("choice3").style.display="block";
-    document.getElementById("btnSubmit3").style.display="block"; 
+	document.getElementById("choice3").style.display="block";
+	document.getElementById("btnSubmit3").style.display="block";
   }
 
   else
   {
-    myQuestion.innerHTML === "Please look a bit more closely at the options!"
+	myQuestion.innerHTML === "Please look a bit more closely at the options!"
   }
 }
 
@@ -70,31 +70,31 @@ function getChoice3 () {
 
   if (stay_go_choice === 'eat')
   {
-    document.body.style.backgroundColor = "brown";
-    document.getElementById("choice-image").src="img/food-500px.jpg";
-    myQuestion.innerHTML = "Light up the stove and break out the dehydrated meal! After you have eaten, finally started and walked for a few miles, you see a large woodpecker perched on a near by tree. Do you keep on walking or break out the binoculars and try to identify it?"
-    document.getElementById("choice3").style.display="none";
-    document.getElementById("btnSubmit3").style.display="none"; 
+	document.body.style.backgroundColor = "brown";
+	document.getElementById("choice-image").src="img/food-500px.jpg";
+	myQuestion.innerHTML = "Light up the stove and break out the dehydrated meal! After you have eaten, finally started and walked for a few miles, you see a large woodpecker perched on a near by tree. Do you keep on walking or break out the binoculars and try to identify it?"
+	document.getElementById("choice3").style.display="none";
+	document.getElementById("btnSubmit3").style.display="none";
 
-    document.getElementById("choice4").style.display="block";
-    document.getElementById("btnSubmit4").style.display="block"; 
+	document.getElementById("choice4").style.display="block";
+	document.getElementById("btnSubmit4").style.display="block";
   }
 
   else if(stay_go_choice ==='walk')
   {
-    document.body.style.backgroundColor = "brown";
-    document.getElementById("choice-image").src="img/hiking-500px.jpg";
-    myQuestion.innerHTML = "Lace up your boots, adjust your pack and start walking!After you have walked for a few miles, you see a large woodpecker perched on a near by tree. Do you keep on walking or break out the binoculars and try to identify it?" 
-    document.getElementById("choice3").style.display="none";
-    document.getElementById("btnSubmit3").style.display="none"; 
+	document.body.style.backgroundColor = "brown";
+	document.getElementById("choice-image").src="img/hiking-500px.jpg";
+	myQuestion.innerHTML = "Lace up your boots, adjust your pack and start walking!After you have walked for a few miles, you see a large woodpecker perched on a near by tree. Do you keep on walking or break out the binoculars and try to identify it?"
+	document.getElementById("choice3").style.display="none";
+	document.getElementById("btnSubmit3").style.display="none";
 
-    document.getElementById("choice4").style.display="block";
-    document.getElementById("btnSubmit4").style.display="block"; 
+	document.getElementById("choice4").style.display="block";
+	document.getElementById("btnSubmit4").style.display="block";
   }
 
   else
   {
-    myQuestion.innerHTML === "Please look a bit more closely at the options!"
+	myQuestion.innerHTML === "Please look a bit more closely at the options!"
   }
 }
 
@@ -104,31 +104,31 @@ function getChoice4 () {
 
   if (stay_go_choice === 'ignore')
   {
-    document.body.style.backgroundColor = "DeepSkyBlue";
-    document.getElementById("choice-image").src="img/hiking2-500px.jpg";
-    myQuestion.innerHTML = "After a long day of hiking, you have finally arrived at a campsite. Are you going to get an early night, or do you want to stay up late and look at the stars?"
-    document.getElementById("choice4").style.display="none";
-    document.getElementById("btnSubmit4").style.display="none"; 
+	document.body.style.backgroundColor = "DeepSkyBlue";
+	document.getElementById("choice-image").src="img/hiking2-500px.jpg";
+	myQuestion.innerHTML = "After a long day of hiking, you have finally arrived at a campsite. Are you going to get an early night, or do you want to stay up late and look at the stars?"
+	document.getElementById("choice4").style.display="none";
+	document.getElementById("btnSubmit4").style.display="none";
 
-    document.getElementById("choice5").style.display="block";
-    document.getElementById("btnSubmit5").style.display="block"; 
+	document.getElementById("choice5").style.display="block";
+	document.getElementById("btnSubmit5").style.display="block";
   }
 
   else if(stay_go_choice ==='binoculars')
   {
-    document.body.style.backgroundColor = "DeepSkyBlue";
-    document.getElementById("choice-image").src="img/woodpecker-500px.jpg";
-    myQuestion.innerHTML = "After a long day of hiking, you have finally arrived at a campsite. Are you going to get an early night, or do you want to stay up late and look at the stars?" 
-    document.getElementById("choice4").style.display="none";
-    document.getElementById("btnSubmit4").style.display="none"; 
+	document.body.style.backgroundColor = "DeepSkyBlue";
+	document.getElementById("choice-image").src="img/woodpecker-500px.jpg";
+	myQuestion.innerHTML = "After a long day of hiking, you have finally arrived at a campsite. Are you going to get an early night, or do you want to stay up late and look at the stars?"
+	document.getElementById("choice4").style.display="none";
+	document.getElementById("btnSubmit4").style.display="none";
 
-    document.getElementById("choice5").style.display="block";
-    document.getElementById("btnSubmit5").style.display="block"; 
+	document.getElementById("choice5").style.display="block";
+	document.getElementById("btnSubmit5").style.display="block";
   }
 
   else
   {
-    myQuestion.innerHTML === "Please look a bit more closely at the options!"
+	myQuestion.innerHTML === "Please look a bit more closely at the options!"
   }
 }
 
@@ -138,31 +138,31 @@ function getChoice5 () {
 
   if (stay_go_choice === 'sleep')
   {
-    document.body.style.backgroundColor = "MidnightBlue ";
-    document.getElementById("choice-image").src="img/sleep-500px.jpg";
-    myQuestion.innerHTML = "Congratulations - you've survived your first day! Are you going home, or do you want to carry on with the adventure?"
-    document.getElementById("choice5").style.display="none";
-    document.getElementById("btnSubmit5").style.display="none"; 
+	document.body.style.backgroundColor = "MidnightBlue ";
+	document.getElementById("choice-image").src="img/sleep-500px.jpg";
+	myQuestion.innerHTML = "Congratulations - you've survived your first day! Are you going home, or do you want to carry on with the adventure?"
+	document.getElementById("choice5").style.display="none";
+	document.getElementById("btnSubmit5").style.display="none";
 
-    document.getElementById("choice6").style.display="block";
-    document.getElementById("btnSubmit6").style.display="block"; 
+	document.getElementById("choice6").style.display="block";
+	document.getElementById("btnSubmit6").style.display="block";
   }
 
   else if(stay_go_choice ==='stars')
   {
-    document.body.style.backgroundColor = "MidnightBlue ";
-    document.getElementById("choice-image").src="img/stars-500px.jpg";
-    myQuestion.innerHTML = "Congratulations - you've survived your first day! Are you going home, or do you want to carry on with the adventure?" 
-    document.getElementById("choice5").style.display="none";
-    document.getElementById("btnSubmit5").style.display="none"; 
+	document.body.style.backgroundColor = "MidnightBlue ";
+	document.getElementById("choice-image").src="img/stars-500px.jpg";
+	myQuestion.innerHTML = "Congratulations - you've survived your first day! Are you going home, or do you want to carry on with the adventure?"
+	document.getElementById("choice5").style.display="none";
+	document.getElementById("btnSubmit5").style.display="none";
 
-    document.getElementById("choice6").style.display="block";
-    document.getElementById("btnSubmit6").style.display="block"; 
+	document.getElementById("choice6").style.display="block";
+	document.getElementById("btnSubmit6").style.display="block";
   }
 
   else
   {
-    myQuestion.innerHTML === "Please look a bit more closely at the options!"
+	myQuestion.innerHTML === "Please look a bit more closely at the options!"
   }
 }
 
@@ -172,31 +172,31 @@ function getChoice6 () {
 
   if (stay_go_choice === 'return')
   {
-    document.body.style.backgroundColor = "NavajoWhite ";
-    document.getElementById("choice-image").src="img/car-500px.jpg";
-    myQuestion.innerHTML = "Safe travels home!"
-    document.getElementById("choice6").style.display="none";
-    document.getElementById("btnSubmit6").style.display="none"; 
+	document.body.style.backgroundColor = "NavajoWhite ";
+	document.getElementById("choice-image").src="img/car-500px.jpg";
+	myQuestion.innerHTML = "Safe travels home!"
+	document.getElementById("choice6").style.display="none";
+	document.getElementById("btnSubmit6").style.display="none";
 
-    // document.getElementById("choice7").style.display="block";
-    // document.getElementById("btnSubmit7").style.display="block"; 
+	// document.getElementById("choice7").style.display="block";
+	// document.getElementById("btnSubmit7").style.display="block";
   }
 
   else if(stay_go_choice ==='continue')
   {
-    document.body.style.backgroundColor = "NavajoWhite ";
-    document.getElementById("choice-image").src="img/continue-500px.jpg";
-    myQuestion.innerHTML = "Good for you!" 
-    document.getElementById("choice6").style.display="none";
-    document.getElementById("btnSubmit6").style.display="none"; 
+	document.body.style.backgroundColor = "NavajoWhite ";
+	document.getElementById("choice-image").src="img/continue-500px.jpg";
+	myQuestion.innerHTML = "Good for you!"
+	document.getElementById("choice6").style.display="none";
+	document.getElementById("btnSubmit6").style.display="none";
 
-    // document.getElementById("choice7").style.display="block";
-    // document.getElementById("btnSubmit7").style.display="block"; 
+	// document.getElementById("choice7").style.display="block";
+	// document.getElementById("btnSubmit7").style.display="block";
   }
 
   else
   {
-    myQuestion.innerHTML === "Please look a bit more closely at the options!"
+	myQuestion.innerHTML === "Please look a bit more closely at the options!"
   }
 }
 
