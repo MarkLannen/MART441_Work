b80e63159ee253e900ce0949cd473c4c88d53fd7
Mark Lannen <mark1.lannen@umontana.edu>
2019-02-14 20:32:53 -0700

    commit before trying to add new array

--- HW-5/scripts/script.js
+++ HW-5/scripts/script.js
@@ -1,6 +1,12 @@
-var startImage = ["image1", "image2", "image3", "image4", "image5", "image6","image7", "image8", "image9", "image10", "image11", "image12"];
+var startImageArray = [];
+
+
+
+var startImage = ["image1", "image2", "image3", "image4", "image5", "image6","image7", "image8", "image9", "image10"];
 
 var startImagePath = 'img/hidden-image.jpg';
+
+var hiddenImages = [];
 
 /* addStartCard function populates array with 'blank image.'*/
 function addStartCard() {
@@ -12,3 +18,15 @@ function addStartCard() {
 }
 
 // var count = [0,0,0,0,0,0]; Need 6 slots. 2 for each picture. 
+
+function createHiddenImageArray() {
+
+    var hiddenImagePath = ["img/city.jpg", "img/desert.jpg", "img/forest.jpg", "img/mountains.jpg", "img/ocean.jpg"]
+
+
+
+
+
+
+
+}
