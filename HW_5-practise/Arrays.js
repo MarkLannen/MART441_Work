34089ef5a4433d1d8bfaf66a881e9a1182cac2c2
Mark Lannen <mark1.lannen@umontana.edu>
2019-02-16 16:33:19 -0700

    Removed extra files

--- HW_5-practise/Arrays.js
+++
@@ -1,9 +0,0 @@
-function printArray()
-{
-    var friends = "George,Grace,Ella,John,Meghan,Ben";
-    var separateFriends = friends.split(",");
-    var allFriends = "";
-    for(var i = 0; i < separateFriends.length; i++)
-    {
-        allFriends += separateFriends[i] + "<br>";
-    }
