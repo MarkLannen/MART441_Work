b09ccd2841b9dbe988ec323a9a578354c50c2c86
Mark Lannen <mark1.lannen@umontana.edu>
2019-03-02 13:52:41 -0700

    updated

--- HW-7/scripts/script.js
+++ HW-7/scripts/script.js
@@ -1,28 +1,50 @@
+var myViewFinderArray = new Array();
+
 class ViewFinderObjects
 {
-    constructor(title, description, author, year)
+    constructor(title, image, description, author, year)
     {
         this.title = title;
+        this.image = image;
         this.description = description;
         this.author = author;
         this.year = year;
     }
 
-    toString()
+    toStringTitle()
     {
         return "Title: " + this.title;
+    }
+
+    toStringDescription()
+    {
+        return "Description: " + this.description;
     }
 
     get theTitle()
     { 
         return this.title;
     }
+
+    get theImage()
+    { 
+        return this.image;
+    }
+}
+
+function initializeArray()
+{
+    var myViewFinderTitle = new ViewFinderObjects("2 Bison");
+    var myViewFinderDescription = new ViewFinderObjects("Black and white photograph of 2 bison");
+    myViewFinderArray.push(myViewFinderTitle);
+    myViewFinderArray.push(myViewFinderDescription);
 }
 
 function getInformation()
 {
-    var myViewFinder = new ViewFinderObjects("2 Bison");
     // console.log(myViewFinder.theTitle);
     // console.log(myViewFinder.toString());
-    document.getElementById("title").innerHTML = myViewFinder.toString();
+    document.getElementById("title").innerHTML = myViewFinderArray[0].toStringTitle();
+    document.getElementById("description").innerHTML = myViewFinderArray[0].toStringDescription();
+    
 }
