b16ef21fe4026117df8af552522f4d1a987e8011
Mark Lannen <mark1.lannen@umontana.edu>
2019-03-03 16:59:06 -0700

    changed image size and updated styles

--- HW-7/scripts/script.js
+++ HW-7/scripts/script.js
@@ -35,20 +35,15 @@ class ViewFinderObjects
     {
         return "Year: " + this.year;
     }
-
-    // get theTitle()
-    // { 
-    //     return this.title;
-    // }
 }
 
 function initializeArray()
 {
-    var myViewFinder1 = new ViewFinderObjects("img/2BisonHorizon-750px.jpg", "2 Bison", "Black and white photograph of 2 bison", "Mark Lannen", "2014");
-    var myViewFinder2 = new ViewFinderObjects("img/Bitterroots-750px.jpg", "Bitterroot macro", "Macro photograph of bitterroot flowers", "Mark Lannen", "2015");
-    var myViewFinder3 = new ViewFinderObjects("img/FlatheadLake-750px.jpg", "Flathead B&W", "Black and white long exposure from south shore of Flathead Lake", "Mark Lannen", "2012");
-    var myViewFinder4 = new ViewFinderObjects("img/NorthCrowBarn-750px.jpg", "North Crow barn timestack", "Timestack of barn near North Crow Creek - Mission Mountains", "Mark Lannen", "2013");
-    var myViewFinder5 = new ViewFinderObjects("img/SonyokMtn-750px.jpg", "Sonyok Mountain timestack", "Timestack of sunset from Sonyok Mountain", "Mark Lannen", "2014");
+    var myViewFinder1 = new ViewFinderObjects("img/2BisonHorizon-500px.jpg", "2 Bison", "Black and white photograph of 2 bison", "Mark Lannen", "2014");
+    var myViewFinder2 = new ViewFinderObjects("img/Bitterroots-500px.jpg", "Bitterroot macro", "Macro photograph of bitterroot flowers", "Mark Lannen", "2015");
+    var myViewFinder3 = new ViewFinderObjects("img/FlatheadLake-500px.jpg", "Flathead B&W", "Black and white long exposure from south shore of Flathead Lake", "Mark Lannen", "2012");
+    var myViewFinder4 = new ViewFinderObjects("img/NorthCrowBarn-500px.jpg", "North Crow barn timestack", "Timestack of barn near North Crow Creek - Mission Mountains", "Mark Lannen", "2013");
+    var myViewFinder5 = new ViewFinderObjects("img/SonyokMtn-500px.jpg", "Sonyok Mountain timestack", "Timestack of sunset from Sonyok Mountain", "Mark Lannen", "2014");
 
     myViewFinderArray.push(myViewFinder1);
     myViewFinderArray.push(myViewFinder2);
@@ -60,8 +55,6 @@ function initializeArray()
 function getRandomIndex(a){
     return Math.floor(Math.random(a) * 5);  
 }
-
-
 
 function getInformation()
 {
