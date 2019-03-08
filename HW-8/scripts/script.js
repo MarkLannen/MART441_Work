efb9d3da28ed1bc81d31b37ed6f8d310ce5a020c
Mark Lannen <mark1.lannen@umontana.edu>
2019-03-07 22:01:53 -0700

    Update script.js

--- HW-8/scripts/script.js
+++ HW-8/scripts/script.js
@@ -3,8 +3,15 @@ console.log("World!")
 $(document).ready(function() {
     
     $("button").click(function(){
-        alert("hi!")
+        
+        moveSquare();
+        
     });
 
-
 });
+
+function moveSquare(){
+    
+    $("#square1").animate({left:250}).animate({top:500}).animate({left:50}).animate({top:150});
+    
+}
