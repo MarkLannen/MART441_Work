ae755895c5dd78dc6c3c4ccdf86fc7836d7db01f
Mark Lannen <mark1.lannen@umontana.edu>
2019-03-09 21:07:03 -0700

    removed commented code

--- HW-8/scripts/script.js
+++ HW-8/scripts/script.js
@@ -40,6 +40,11 @@ $(document).ready(function() {
 
 });
 
+
+
+
+
+
 // function moveSquare(){
     
 //     $("#square1").animate({left:250}).animate({top:500}).animate({left:50}).animate({top:150});
