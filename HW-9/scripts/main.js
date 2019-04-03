79bad816e439f649cbf073900ef76503f45c5562
Mark Lannen <mark1.lannen@umontana.edu>
2019-04-02 20:43:36 -0600

    updated javascript files

--- HW-9/scripts/main.js
+++ HW-9/scripts/main.js
@@ -1,8 +1,23 @@
  $(document).ready(function () {
             $("button").click(function () {
-                $("#indianStatesInfo").load("data/indianStates.json", function(responseText){
+                $("#indianStateName").load("data/indianStates.json", function(responseText){
                     var indianState = JSON.parse(responseText);
-                    $("#indianStatesInfo").html(indianState.states[1].state.state_id);
+                    console.log(indianState.states);
+
+                    var htmlString = "";
+                    for (var j = 0; j < indianState.states.length; j ++) {
+                        console.log(j);
+                        htmlString = htmlString + "<tr>" + "<td>" + indianState.states[j].state.state_name + "</td>" + "<td>" + indianState.states[j].state.state_id + "</td>" + "</tr><br>"
+                        //$("#indianStateName").html("Indian State name: " + indianState.states[j].state.state_name);
+                        //$("#indianStateAbbrev").html("Indian State name: " + indianState.states[j].state.state_id);
+                    }
+
+                    $("#indianStateName").html(htmlString);
+
                 });
             });
         });
+
+
+
+//$("#indianStateName").html("Indian State name: " + indianState.states[0].state.state_name + "<br>Abbreviation: " + indianState.states[0].state.state_id);
