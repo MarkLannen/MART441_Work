6a9ca397d5e17f503f7ac89d43cac622a2b4711e
Mark Lannen <mark1.lannen@umontana.edu>
2019-04-02 21:08:14 -0600

    added readme

---
+++ HW-9/README.md
@@ -0,0 +1,20 @@
+# Mark Lannen MART 441
+## HW-9 Readme
+
+### Process
+Apologies that this is late! I read and re-read the material and by the time I really
+started on the homework I ran in to some roadblocks and couldn't really get them
+sorted out until today.
+
+Anyway, I was pleased that I eventually got everything to work and have at least a
+basic grasp on jquery, AJAX etc.
+
+Once I understood the syntax for getting the JSON file info and parsing, the biggest
+challenge was to get it to display properly in a table. The display is not all that interesting,
+but I was mostly focused on getting everything to work. I was particularly pleased
+that I managed to loop through the JSON file and extract the bits that I wanted and display
+them in a somewhat reasonable way.
+
+I know what I have done here is pretty basic, but I think I have understood how it
+all works together and hopefully I can incrementally add complexity and still get
+the functionality I want.
--- HW-9/scripts/main.js
+++ HW-9/scripts/main.js
@@ -7,9 +7,7 @@
                     var htmlString = "";
                     for (var j = 0; j < indianState.states.length; j ++) {
                         console.log(j);
-                        htmlString = htmlString + "<tr>" + "<td>" + indianState.states[j].state.state_name + "</td>" + "<td>" + indianState.states[j].state.state_id + "</td>" + "</tr><br>"
-                        //$("#indianStateName").html("Indian State name: " + indianState.states[j].state.state_name);
-                        //$("#indianStateAbbrev").html("Indian State name: " + indianState.states[j].state.state_id);
+                        htmlString = htmlString + "<tr>" + "<td>" + indianState.states[j].state.state_name + "</td>" + "<td>" + indianState.states[j].state.state_id + "</td>" + "</tr><br>";
                     }
 
                     $("#indianStateName").html(htmlString);
@@ -17,7 +15,3 @@
                 });
             });
         });
-
-
-
-//$("#indianStateName").html("Indian State name: " + indianState.states[0].state.state_name + "<br>Abbreviation: " + indianState.states[0].state.state_id);
